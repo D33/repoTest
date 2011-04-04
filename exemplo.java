7cdeaf967c0549757ddb5b262397d7524fa2515a
Dieges Lima <diegeslima@gmail.com>
2011-04-03 22:42:53 -0300

    Add a linha 8

--- exemplo.java
+++ exemplo.java
@@ -1,4 +1,4 @@
-public class exemplo {
+﻿public class exemplo {
 
 	/**
 	 * @param args
@@ -6,6 +6,7 @@ public class exemplo {
 	public static void main(String[] args) {
 		List<ContaSimples> contas = new ArrayList<ContaSimples>();
 		contas.add(new ContaSimples(1,"João","30/02/2002",500.5));
+		contas.add(new ContaSimples(3,"Jana","30/02/2001",500));
 		contas.add(new ContaSimples(2,"Denise","19/08/1899",700.8));
 		for(Iterator<ContaSimples> it = contas.iterator(); it.hasNext();)
 			System.out.println(it.next().toString());
